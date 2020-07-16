5c7b3d2b4cec70cc06c300caa2a789ecbbffad39
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-16 14:29:48 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,13 +7,20 @@ fruit = {
 end
 
 
-def shipping_manifest(the_manifest)
+def shipping_manifest(the_manifest = {})
   the _manifest = {
     "whale bone corsets" => 5,
     "oil paintings" => 3,
     "porcelain vases" => 2
   }
  end
+ 
+ def hello_message(name_parts = {})
+  first_name = name_parts.fetch(:first_name)
+  last_name = name_parts.fetch(:last_name)
+
+  "Hello, #{first_name} #{last_name}"
+end
  
 def retrieval
   shipping_manifest = {
