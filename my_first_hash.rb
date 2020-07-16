9ef36bac02d0f849351b0f8701055fcbd77bd397
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-16 15:24:29 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,7 +25,7 @@ def adding
   shipping_manifest["muskets"] = "2"
   shipping_manifest["gun powder"] = "4"
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
+                  ["gun powder"] = "4""whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3}
 end
