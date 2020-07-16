5a71e344514ded2fd1d165127c0cf339002e1b6e
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-16 14:18:51 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,8 +12,8 @@ end
 def shipping_manifest
   the _manifest = {
     "whale bone corsets" => "5",
-    "porcelain vases" => "2",
-    "oil paintings" => "3"
+     "oil paintings" => "3""porcelain vases" => "2",
+   
   }
 
 end
