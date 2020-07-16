dd216e1ed93adf051db8479506571b6d2f45f3f5
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-16 15:11:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,12 +19,6 @@ def shipping_manifest(the_manifest={})
  
 def retrieval
   shipping_manifest = [5,2,3]
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-
-
 end
 
 def adding
